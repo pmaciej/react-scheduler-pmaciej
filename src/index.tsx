242f42fb6fc8c020b973c03e8c0aa66d3f6f6a9c
pmaciej <63106694+pmaciej@users.noreply.github.com>
2021-11-16 16:07:01 +0100

    Update index.tsx
    Changed global font to Nunito Sans

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import "./index.css";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Nunito Sans"
+  }
+});
+
 
 ReactDOM.render(
   <div style={{ margin: 10 }}>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </div>,
   document.getElementById("root")
 );
